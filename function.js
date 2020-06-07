010121da36fa570776251bc84c8c01dd5177f66f
eltoncampos1 <qazwsx272518>
2020-06-07 15:25:34 -0300

    Inserindo validação do nome

--- function.js
+++ function.js
@@ -24,5 +24,42 @@ $(function(){
 
 
     }
-      
+
+    $('form#form1').submit(function(e){
+        e.preventDefault();
+        var nome = $('input[name=nome]').val();
+        var telefone = $('input[name=telefone]').val();
+        var email = $('input[name=email]').val();
+
+        //contando a quantidade de espaços e os respectivos valores
+        var amount = nome.split(' ').length
+        var splitStr = nome.split(' ');
+        if( amount >= 2){
+            for(var i = 0; i < amount; i++){
+               if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
+                
+               }else{
+                   aplicarCamposInvalido($('input[name=nome]'));
+                   return false;
+               }
+           
+            }
+                  
+        }else{
+            aplicarCamposInvalido($('input[name=nome]'));
+            return false;
+
+        }
+
+        //se chegou até aqui é por que est[a tudo okay!
+
+    })
+
+        function aplicarCamposInvalido(el){
+            el.css('border','2px solid red');
+            el.data('invalido','true')
+            el.val('Campos inválido!')
+        }
+    
+
 });
